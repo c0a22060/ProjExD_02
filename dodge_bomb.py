c494243235a22192cd060e03bb3a6a72b4a2585c
C0A22060 <c0a22060c8@edu.teu.ac.jp>
2023-06-20 16:22:45 +0900

    追加機能1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -10,6 +10,37 @@ vector = {
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (+5, 0),
 }
+
+def direction_kk(mv, img):  #追加機能1
+    """
+    それぞれの回転反転した画像を代入
+    方向に合わせて辞書化する
+    0.0の時は何も代入しないのでFalse
+    """
+    kk_img_ogin = pg.image.load("ex02/fig/3.png")
+    kk_img = pg.transform.flip(kk_img_ogin, True, False)
+    kk_img1 = pg.transform.rotozoom(kk_img_ogin, 0, 2.0)
+    kk_img2 = pg.transform.rotozoom(kk_img_ogin, 45, 2.0)
+    kk_img3 = pg.transform.rotozoom(kk_img_ogin, -45, 2.0)
+    kk_img4 = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img5 = pg.transform.rotozoom(kk_img, 45, 2.0)
+    kk_img6 = pg.transform.rotozoom(kk_img, -45, 2.0)
+    kk_img7 = pg.transform.rotozoom(kk_img, 90, 2.0)
+    kk_img8 = pg.transform.rotozoom(kk_img, -90, 2.0)
+    if mv == [0, 0]:
+        return False
+    kk_img_way = {
+        (+5, 0):kk_img4,
+        (+5, +5):kk_img6,
+        (0, +5):kk_img8,
+        (-5, +5):kk_img2,
+        (-5, 0):kk_img1,
+        (-5, -5):kk_img3,
+        (0, -5):kk_img7,
+        (+5, -5):kk_img5,
+    }
+    return kk_img_way[tuple(mv)]
+
 def inout(Rect:pg.Rect) -> tuple[bool, bool]:
     """
     こうかとんが画面の中か外かを判定する関数
@@ -49,20 +80,33 @@ def main():
             
         if kk_rct.colliderect(bd_rct):
             print("ゲームオーバー")
-            return 
-        
+            return  #ゲームオーバー処理
+            
         key_1st = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k, mv in vector.items():
             if key_1st[k]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+        if tmr == 0:
+            kk_img_new = kk_img
+        if direction_kk(sum_mv, kk_img) != False:
+            kk_img_new = direction_kk(sum_mv, kk_img)
+        """
+        追加機能1
+        こうかとんの画像を
+        direction_kkにsum_mvを代入し帰ってきた画像を代入する
+        向きを残るようにしたいので0, 0を入れても更新されない
+        最初だけ画像を代入する
+        """
+        
         kk_rct.move_ip(sum_mv)  #練習3
         if inout(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, kk_rct)
+        screen.blit(kk_img_new, kk_rct)
+
         bd_rct.move_ip(vx, vy)  #練習3
         yoko, tate = inout(bd_rct)
         if not yoko:
